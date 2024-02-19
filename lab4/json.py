787cce0c56eeb94394435e56bdd8aae1af17d1bf
KamilaAkhmetova <k_akhmetova@kbtu.kz>
2024-02-19 22:12:27 +0600

    'upd'

--- lab4/json.py
+++ lab4/json.py
@@ -1,9 +1,10 @@
 import json
 print("Interface Status")
 print("================================================================================")
-print("DN                                                 Description           Speed    MTU ")
+print("DN                                                 Description           Speed    MTU  ")
 print("-------------------------------------------------- --------------------  ------  ------")
+
 with open("sample-data.json", "r") as js_file:
     j = json.load(js_file)
 for x in j["imdata"]:
-    print(f"{x["l1PhysIf"]["attributes"]["dn"]}                            {x["l1PhysIf"]["attributes"]["speed"]}   {x["l1PhysIf"]["attributes"]["mtu"]}")
+    print(f"{x["l1PhysIf"]["attributes"]["dn"]}                              {x["l1PhysIf"]["attributes"]["speed"]}   {x["l1PhysIf"]["attributes"]["mtu"]}")
